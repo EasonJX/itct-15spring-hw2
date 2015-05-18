bfa8dee71ae5fea41a0dccfb07b32dff58590875
Shao-Chuan Lee <lsc36x@gmail.com>
2015-05-18 21:05:22 +0800

    bugfix

--- parsers.c
+++ parsers.c
@@ -9,7 +9,7 @@ void parse_APP0(APP0Segment *seg)
 void parse_DQT(DQTSegment *seg)
 {
     assert(seg->PqTq >> 4 == 0);
-    printf("Tq = %d\n", seg->PqTq | 0xf);
+    printf("Tq = %d\n", seg->PqTq & 0xf);
     printf("Quantization table:\n");
     uint8_t mat[8][8];
     zigzag_to_mat(seg->qt_zz, mat);
